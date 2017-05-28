f27fddb273ded358def7be9fbc6f18af3e33940e
Syngmaster <max6361@mail.ru>
2017-05-28 13:24:21 +0100

    Initial Commit

---
+++ ClientServerAPI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClientServerAPI
+//
+//  Created by Syngmaster on 28/05/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClientServerAPI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClientServerAPI
+//
+//  Created by Syngmaster on 28/05/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
